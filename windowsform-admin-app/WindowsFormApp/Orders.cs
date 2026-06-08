670352c0cccb4623bf518039501d4e7f6ea9b34a
J.A.K.S.Gunathilaka <88920956+rootks@users.noreply.github.com>
2026-06-08 23:57:55 +0530

    created product management form

---
+++ windowsform-admin-app/WindowsFormApp/Orders.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WindowsFormsApp9
+{
+    internal class Orders
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double Price { get; set; }
+
+        public double Total { get; set; }
+
+        public string Status { get; set; }
+    }
+}
