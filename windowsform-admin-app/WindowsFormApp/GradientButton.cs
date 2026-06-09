e169771ff1936d06ad5633b8be8753c6ee744b67
J.A.K.S.Gunathilaka <88920956+rootks@users.noreply.github.com>
2026-06-09 17:38:10 +0530

    add gradient Winform Tool

---
+++ windowsform-admin-app/WindowsFormApp/GradientButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+public class GradientButton : Button
+{
+    public Color Color1 { get; set; } = Color.DeepSkyBlue;
+    public Color Color2 { get; set; } = Color.MediumPurple;
+
+    public GradientButton()
+    {
+        FlatStyle = FlatStyle.Flat;
+        FlatAppearance.BorderSize = 0;
+        ForeColor = Color.White;
+        Font = new Font("Segoe UI", 10, FontStyle.Bold);
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        Graphics g = e.Graphics;
+
+        using (LinearGradientBrush brush =
+            new LinearGradientBrush(
+                ClientRectangle,
+                Color1,
+                Color2,
+                LinearGradientMode.Horizontal))
+        {
+            g.FillRectangle(brush, ClientRectangle);
+        }
+
+        TextRenderer.DrawText(
+            g,
+            Text,
+            Font,
+            ClientRectangle,
+            ForeColor,
+            TextFormatFlags.HorizontalCenter |
+            TextFormatFlags.VerticalCenter);
+    }
+}
---
+++ windowsform-admin-app/WindowsFormApp/GradientText.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+public class GradienText : Label
+{
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        using (LinearGradientBrush brush =
+            new LinearGradientBrush(
+                this.ClientRectangle,
+                Color.DeepSkyBlue,
+                Color.MediumPurple,
+                0f))
+        {
+            e.Graphics.DrawString(
+                this.Text,
+                this.Font,
+                brush,
+                0,
+                0);
+        }
+    }
+}
