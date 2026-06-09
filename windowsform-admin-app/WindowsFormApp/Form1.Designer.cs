cb4fc1bbf73d87a3d1fdd76233ea9159393404f5
J.A.K.S.Gunathilaka <88920956+rootks@users.noreply.github.com>
2026-06-09 14:06:37 +0530

    created Form1 which is connected to all other Forms
    Create your forms by using name which are in Form1 code.

---
+++ windowsform-admin-app/WindowsFormApp/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sidebar = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnMenu = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pnProducts = new System.Windows.Forms.Panel();
+            this.pnOrders = new System.Windows.Forms.Panel();
+            this.pnBilling = new System.Windows.Forms.Panel();
+            this.pnUsers = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.pnSalary = new System.Windows.Forms.Panel();
+            this.pnLogout = new System.Windows.Forms.Panel();
+            this.sidebarTimer = new System.Windows.Forms.Timer(this.components);
+            this.menuButton = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.sidebar.SuspendLayout();
+            this.pnMenu.SuspendLayout();
+            this.pnProducts.SuspendLayout();
+            this.pnOrders.SuspendLayout();
+            this.pnBilling.SuspendLayout();
+            this.pnUsers.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.pnSalary.SuspendLayout();
+            this.pnLogout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.menuButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sidebar
+            // 
+            this.sidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(0)))), ((int)(((byte)(184)))));
+            this.sidebar.Controls.Add(this.pnMenu);
+            this.sidebar.Controls.Add(this.pnProducts);
+            this.sidebar.Controls.Add(this.pnOrders);
+            this.sidebar.Controls.Add(this.pnBilling);
+            this.sidebar.Controls.Add(this.pnUsers);
+            this.sidebar.Controls.Add(this.panel6);
+            this.sidebar.Controls.Add(this.pnSalary);
+            this.sidebar.Controls.Add(this.pnLogout);
+            this.sidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidebar.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.sidebar.Location = new System.Drawing.Point(0, 0);
+            this.sidebar.MaximumSize = new System.Drawing.Size(245, 678);
+            this.sidebar.MinimumSize = new System.Drawing.Size(60, 486);
+            this.sidebar.Name = "sidebar";
+            this.sidebar.Size = new System.Drawing.Size(245, 609);
+            this.sidebar.TabIndex = 11;
+            // 
+            // pnMenu
+            // 
+            this.pnMenu.Controls.Add(this.label3);
+            this.pnMenu.Controls.Add(this.menuButton);
+            this.pnMenu.Location = new System.Drawing.Point(3, 3);
+            this.pnMenu.Name = "pnMenu";
+            this.pnMenu.Size = new System.Drawing.Size(242, 57);
+            this.pnMenu.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(78, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 21);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Menu";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // pnProducts
+            // 
+            this.pnProducts.Controls.Add(this.button1);
+            this.pnProducts.Location = new System.Drawing.Point(3, 66);
+            this.pnProducts.Name = "pnProducts";
+            this.pnProducts.Size = new System.Drawing.Size(253, 51);
+            this.pnProducts.TabIndex = 1;
+            // 
+            // pnOrders
+            // 
+            this.pnOrders.Controls.Add(this.button2);
+            this.pnOrders.Location = new System.Drawing.Point(3, 123);
+            this.pnOrders.Name = "pnOrders";
+            this.pnOrders.Size = new System.Drawing.Size(253, 51);
+            this.pnOrders.TabIndex = 2;
+            // 
+            // pnBilling
+            // 
+            this.pnBilling.Controls.Add(this.button3);
+            this.pnBilling.Location = new System.Drawing.Point(3, 180);
+            this.pnBilling.Name = "pnBilling";
+            this.pnBilling.Size = new System.Drawing.Size(253, 51);
+            this.pnBilling.TabIndex = 3;
+            // 
+            // pnUsers
+            // 
+            this.pnUsers.Controls.Add(this.button4);
+            this.pnUsers.Location = new System.Drawing.Point(3, 237);
+            this.pnUsers.Name = "pnUsers";
+            this.pnUsers.Size = new System.Drawing.Size(253, 51);
+            this.pnUsers.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.button5);
+            this.panel6.Location = new System.Drawing.Point(3, 294);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(253, 51);
+            this.panel6.TabIndex = 5;
+            // 
+            // pnSalary
+            // 
+            this.pnSalary.Controls.Add(this.button6);
+            this.pnSalary.Location = new System.Drawing.Point(3, 351);
+            this.pnSalary.Name = "pnSalary";
+            this.pnSalary.Size = new System.Drawing.Size(253, 51);
+            this.pnSalary.TabIndex = 6;
+            // 
+            // pnLogout
+            // 
+            this.pnLogout.Controls.Add(this.button7);
+            this.pnLogout.Location = new System.Drawing.Point(3, 408);
+            this.pnLogout.Name = "pnLogout";
+            this.pnLogout.Size = new System.Drawing.Size(253, 51);
+            this.pnLogout.TabIndex = 7;
+            // 
+            // sidebarTimer
+            // 
+            this.sidebarTimer.Interval = 1;
+            this.sidebarTimer.Tick += new System.EventHandler(this.sidebarTimer_Tick);
+            // 
+            // menuButton
+            // 
+            this.menuButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuButton.Image = global::WindowsFormsApp9.Properties.Resources.Menu5;
+            this.menuButton.Location = new System.Drawing.Point(13, 10);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(35, 31);
+            this.menuButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.menuButton.TabIndex = 0;
+            this.menuButton.TabStop = false;
+            this.menuButton.Click += new System.EventHandler(this.menuButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = global::WindowsFormsApp9.Properties.Resources.Pizza5;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(-10, -4);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(272, 59);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "                Products";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = global::WindowsFormsApp9.Properties.Resources.Add_Shopping_Cart5;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(-10, -4);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.button2.Size = new System.Drawing.Size(272, 59);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "                Orders";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Image = global::WindowsFormsApp9.Properties.Resources.POS_Terminal5;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(-10, -4);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.button3.Size = new System.Drawing.Size(272, 59);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "                Billing";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Image = global::WindowsFormsApp9.Properties.Resources.People5;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(-10, -4);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.button4.Size = new System.Drawing.Size(272, 59);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "                Users";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Image = global::WindowsFormsApp9.Properties.Resources.Total_Sales5;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(-10, -4);
+            this.button5.Name = "button5";
+            this.button5.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.button5.Size = new System.Drawing.Size(272, 59);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "                Progress";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Image = global::WindowsFormsApp9.Properties.Resources.Receive_Dollar5;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(-10, -4);
+            this.button6.Name = "button6";
+            this.button6.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.button6.Size = new System.Drawing.Size(272, 59);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "                Salary";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Image = global::WindowsFormsApp9.Properties.Resources.Logout6;
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.Location = new System.Drawing.Point(-10, -4);
+            this.button7.Name = "button7";
+            this.button7.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.button7.Size = new System.Drawing.Size(272, 59);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "                Logout";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 609);
+            this.Controls.Add(this.sidebar);
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.sidebar.ResumeLayout(false);
+            this.pnMenu.ResumeLayout(false);
+            this.pnMenu.PerformLayout();
+            this.pnProducts.ResumeLayout(false);
+            this.pnOrders.ResumeLayout(false);
+            this.pnBilling.ResumeLayout(false);
+            this.pnUsers.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.pnSalary.ResumeLayout(false);
+            this.pnLogout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.menuButton)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel sidebar;
+        private System.Windows.Forms.Panel pnMenu;
+        private System.Windows.Forms.Panel pnProducts;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnOrders;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel pnBilling;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel pnUsers;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel pnSalary;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel pnLogout;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox menuButton;
+        private System.Windows.Forms.Timer sidebarTimer;
+    }
+}
+
---
+++ windowsform-admin-app/WindowsFormApp/Form1.cs
@@ -0,0 +1,321 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using MongoDB.Bson;
+
+
+
+namespace WindowsFormsApp9
+{
+
+    public partial class Form1 : Form
+    {
+
+        bool sidebarExpand;
+        ProductForm products;
+        OrderForm orders;
+        UsersForm users;
+        SalaryForm salary;
+        ProgressForm progress;
+        BillingForm billing;
+
+        Dashbord dashbord;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private string selectedCustomerId = ""; //selectRowID
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (dashbord == null)
+            {
+                dashbord = new Dashbord();
+                dashbord.FormClosed += Dashbord_FormClosed;
+                dashbord.MdiParent = this;
+                dashbord.Dock = DockStyle.Fill;
+                dashbord.Show();
+            }
+            else
+            {
+                dashbord.Activate();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /* private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                var client = new MongoClient(
+                    "YOUR_CONNECTION_STRING");
+
+                var db = client.GetDatabase("DeliveryAppDB");
+
+                MessageBox.Show("Connected Successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }   
+        }   */
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnProducts_Click(object sender, EventArgs e)
+        {
+            
+           /* ProductForm form = new ProductForm();
+            form.StartPosition = FormStartPosition.CenterScreen;
+            this.Hide();
+            form.Show();    */
+            
+        }
+
+        private void sidebarTimer_Tick(object sender, EventArgs e)
+        {
+             
+            if (sidebarExpand)
+            {
+                sidebar.Width -= 10;
+                if (sidebar.Width == sidebar.MinimumSize.Width)
+                {
+                    sidebarExpand = false;
+                    sidebarTimer.Stop();
+
+                    pnMenu.Width = sidebar.Width;
+                    pnBilling.Width = sidebar.Width;
+                    pnLogout.Width = sidebar.Width;
+                    pnOrders.Width = sidebar.Width;
+                    pnProducts.Width = sidebar.Width;
+                    pnSalary.Width = sidebar.Width;
+                    pnUsers.Width = sidebar.Width;
+                    panel6.Width = sidebar.Width;
+                }
+            }
+            else
+            {
+                sidebar.Width += 15;
+                if (sidebar.Width == sidebar.MaximumSize.Width)
+                {
+                    sidebarExpand = true;
+                    sidebarTimer.Stop();
+
+                    pnMenu.Width = sidebar.Width;
+                    pnBilling.Width = sidebar.Width;
+                    pnLogout.Width = sidebar.Width;
+                    pnOrders.Width = sidebar.Width;
+                    pnProducts.Width = sidebar.Width;
+                    pnSalary.Width = sidebar.Width;
+                    pnUsers.Width = sidebar.Width;
+                    panel6.Width = sidebar.Width;
+                }
+            }
+        }
+
+        private void menuButton_Click(object sender, EventArgs e)
+        {
+            sidebarTimer.Start();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (products == null)
+            {
+                products = new ProductForm();
+                products.FormClosed += Products_FormClosed;
+                products.MdiParent = this;
+                products.Dock= DockStyle.Fill;
+                products.Show();
+            }
+            else
+            {
+                products.Activate();
+            }
+        }
+
+        private void Products_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            products = null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (orders == null)
+            {
+                orders = new OrderForm();
+                orders.FormClosed += Order_FormClosed;
+                orders.MdiParent = this;
+                orders.Dock = DockStyle.Fill;
+                orders.Show();
+            }
+            else
+            {
+                orders.Activate();
+            }
+        }
+        private void Order_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            orders = null;
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (users == null)
+            {
+                users = new UsersForm();
+                users.FormClosed += Users_FormClosed;
+                users.MdiParent = this;
+                users.Dock = DockStyle.Fill;
+                users.Show();
+            }
+            else
+            {
+                users.Activate();
+            }
+        }
+
+        private void Users_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            users = null;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (salary == null)
+            {
+                salary = new SalaryForm();
+                salary.FormClosed += Salary_FormClosed;
+                salary.MdiParent = this;
+                salary.Dock = DockStyle.Fill;
+                salary.Show();
+            }
+            else
+            {
+                salary.Activate();
+            }
+        }
+
+        private void Salary_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            salary = null;
+        }
+
+        private void Dashbord_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            dashbord = null;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            if (dashbord == null)
+            {
+                dashbord = new Dashbord();
+                dashbord.FormClosed += Dashbord_FormClosed;
+                dashbord.MdiParent = this;
+                dashbord.Dock = DockStyle.Fill;
+                dashbord.Show();
+            }
+            else
+            {
+                dashbord.Activate();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (progress == null)
+            {
+                progress = new ProgressForm();
+                progress.FormClosed += Progress_FormClosed;
+                progress.MdiParent = this;
+                progress.Dock = DockStyle.Fill;
+                progress.Show();
+            }
+            else
+            {
+                progress.Activate();
+            }
+        }
+
+        private void Progress_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            progress = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (billing == null)
+            {
+                billing = new BillingForm();
+                billing.FormClosed += Billing_FormClosed;
+                billing.MdiParent = this;
+                billing.Dock = DockStyle.Fill;
+                billing.Show();
+            }
+            else
+            {
+                billing.Activate();
+            }
+        }
+        private void Billing_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            billing = null;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            LoginForm login = new LoginForm();
+            login.Show();
+            this.Hide();
+        }
+    }
+
+        /* public class MongoDBConnection
+        {
+            private static string connectionUri =
+                "mongodb+srv://admin:yourpassword@cluster0.xxxxx.mongodb.net/?retryWrites=true&w=majority";
+
+            private static MongoClient client =
+                new MongoClient(connectionUri);
+
+            public static IMongoDatabase database =
+                client.GetDatabase("DeliveryAppDB"); 
+        }  */
+    
+}
